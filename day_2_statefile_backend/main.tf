03bc7691f66f3b4e79b2c73c5369bbdc8f776725
supraja <suprajaperi28@gmail.com>
2026-06-03 22:37:59 +0530

    dev1 work

---
+++ day_2_statefile_backend/main.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags={
+        Name = "MY-VPC"
+    }
+  
+}
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1a"
+    tags ={
+        Name = "Subnet1"
+    }
+}
+resource "aws_subnet" "subnet2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
+    tags ={
+        Name = "Subnet2"
+    }
+}
+ 
